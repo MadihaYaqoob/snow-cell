90f0de19d6b445970e7392a96ee84d2b2553b23a
Khurram Mehmood Minhas <khurram.mehmood@arbisoft.com>
2024-08-07 13:32:28 +0500

    Added missing import

--- src/layouts/dashboards/containerRegistery/index.js
+++ src/layouts/dashboards/containerRegistery/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import Card from "@mui/material/Card";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
